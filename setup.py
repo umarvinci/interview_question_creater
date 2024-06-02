38fbbb23e0bcae6eafb9df566a858a5dca0a6360
umarvinci <umarvinci@gmail.com>
2024-06-02 20:58:03 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = "Generative AI Project",
     version= '0.0.0',
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="umarvinci",
+    author_email="umarvinci@gmail.com",
     packages= find_packages(),
     install_requires = []
 )
